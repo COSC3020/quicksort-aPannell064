cd3003105653fc6a91cd1d553f94564d2946557b
aPannell064 <adamkpannell@gmail.com>
2025-02-17 18:42:28 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function quicksort(array) {
+    var lo, p, hi;
+    // Use a stack of low/high ordered pairs to make it behave recursively. 
+    var bounds = [[0, array.length-1]];
+    while(bounds.length) {
+        lo = bounds[0][0];
+        hi = bounds.shift()[1];
+        p = lo;
+        // Uses the method in Bentley's algorithm for this part
+        for(var i = lo+1; i <= hi; i++) {
+            if(array[i] < array[lo]) {
+                [array[i], array[p]] = [array[++p], array[i]];
+            }
+        }
+        [array[lo], array[p]] = [array[p], array[lo]];
+        if(p+1 < hi) {bounds.unshift([p+1, hi]);}
+        if(p-1 > lo) {bounds.unshift([lo, p-1]);}     
+    }
     return array;
 }
